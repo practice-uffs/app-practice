a87714173f7bc2b5acd6405f9a5fa980eac818c5
Andrew M. Silva <andrewsaxx@gmail.com>
2020-08-19 14:35:20 -0300

    bug(service): ajustando caminho dinâmico

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,25 +1,24 @@
 import MainPage from '../pages/main.f7.html';
+import RightPanelPage from '../pages/right-panel.f7.html';
 
 // Tabs
 import HomePage from '../pages/home.f7.html';
 import EnvPage from '../pages/env.f7.html';
 import ScanPage from '../pages/scan.f7.html';
 import ChatsPage from '../pages/chats.f7.html';
+  import ChatPage from '../pages/chat.f7.html';
 import NewsPage from '../pages/news.f7.html';
 
-import ChatPage from '../pages/chat.f7.html';
-
-import RightPanelPage from '../pages/right-panel.f7.html';
 import IdeasPage from '../pages/ideas.f7.html';
 import ServicesPage from '../pages/services.f7.html';
+  import ServicePage from '../pages/service.f7.html';
 import CoinPage from '../pages/coin.f7.html';
 import NotificationsPage from '../pages/notifications.f7.html';
 import SettingsPage from '../pages/settings.f7.html';
 import AboutPage from '../pages/about.f7.html';
 
-import IdServicePage from '../pages/id_service.f7.html';
-
 import LoginPage from '../pages/login.f7.html';
+
 import StatsPage from '../pages/stats.f7.html';
 import NotFoundPage from '../pages/404.f7.html';
 
@@ -83,18 +82,14 @@ var routes = [
     component: IdeasPage,
   },
 
-    {
-        path: '/id_service/',
-        component: IdServicePage,
-    },
-
-    {
-        path: '/coin/',
-        component: CoinPage,
-    },
   {
     path: '/services/',
     component: ServicesPage,
+  },
+
+  {
+    path: '/services/:id/',
+    component: ServicePage,
   },
 
   {
