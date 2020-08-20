b28d454bb888bffdc8d6fa559531ca185cb03e38
Andrew M. Silva <andrewsaxx@gmail.com>
2020-08-20 11:33:04 -0300

    feat(login): requisição e controle de credenciais

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,3 +1,4 @@
+import InitialPage from '../pages/initial.f7.html';
 import MainPage from '../pages/main.f7.html';
 import RightPanelPage from '../pages/right-panel.f7.html';
 
@@ -26,34 +27,45 @@ import NotFoundPage from '../pages/404.f7.html';
 var routes = [
   {
     path: '/',
-    component: MainPage,
-    tabs: [
-      {
-        path: '/',
-        id: 'home',
-        component: HomePage,
-      },
-      {
-        path: '/env/',
-        id: 'env',
-        component: EnvPage,
-      },
-      {
-        path: '/scan/',
-        id: 'scan',
-        component: ScanPage,
-      },
-      {
-        path: '/chats/',
-        id: 'chats',
-        component: ChatsPage,
-      },
-      {
-        path: '/news/',
-        id: 'news',
-        component: NewsPage,
-      },
-    ],
+    async(routeTo, routeFrom, resolve, reject) {
+      // Checking login session
+      if(localStorage.userCredentials) {
+        resolve({
+          component: MainPage,
+          tabs: [
+            {
+              path: '/',
+              id: 'home',
+              component: HomePage,
+            },
+            {
+              path: '/env/',
+              id: 'env',
+              component: EnvPage,
+            },
+            {
+              path: '/scan/',
+              id: 'scan',
+              component: ScanPage,
+            },
+            {
+              path: '/chats/',
+              id: 'chats',
+              component: ChatsPage,
+            },
+            {
+              path: '/news/',
+              id: 'news',
+              component: NewsPage,
+            },
+          ],
+        });
+      } else {
+        resolve({
+          component: InitialPage,
+        });
+      }
+    },
   },
 
   {
@@ -70,7 +82,9 @@ var routes = [
 
   {
     path: '/login/',
-    component: LoginPage,
+    loginScreen: {
+      component: LoginPage,
+    },
   },
 
   {
