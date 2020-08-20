1523d75a32f56c8c7c758ad07a6209d80744d5e5
Andrew M. Silva <andrewsaxx@gmail.com>
2020-08-20 12:51:19 -0300

    feat(routes): adicionando verificações de login

--- src/js/routes.js
+++ src/js/routes.js
@@ -24,6 +24,24 @@ import LoginPage from '../pages/login.f7.html';
 import StatsPage from '../pages/stats.f7.html';
 import NotFoundPage from '../pages/404.f7.html';
 
+const authenticated = function (to, from, resolve, reject) {
+  if (localStorage.userCredentials) {
+    resolve();
+  } else {
+    reject();
+    this.navigate('/')
+  }
+}
+
+const unauthenticated = function (to, from, resolve, reject) {
+  if (!localStorage.userCredentials) {
+    resolve();
+  } else {
+    reject();
+    this.navigate('/')
+  }
+}
+
 var routes = [
   {
     path: '/',
@@ -57,9 +75,15 @@ var routes = [
     async(routeTo, routeFrom, resolve, reject) {
       // Checking login session
       if(localStorage.userCredentials) {
-        resolve({component: MainPage});
+        resolve({
+          component: MainPage,
+          beforeEnter: authenticated,
+        });
       } else {
-        resolve({component: InitialPage});
+        resolve({
+          component: InitialPage,
+          beforeEnter: unauthenticated,
+        });
       }
     },
   },
@@ -67,13 +91,15 @@ var routes = [
   {
     path: '/chats/:id/',
     component: ChatPage,
+    beforeEnter: authenticated,
   },
 
   {
     path: '/right-panel/',
     panel: {
       component: RightPanelPage,
-    }
+    },
+    beforeEnter: authenticated,
   },
 
   {
@@ -81,46 +107,55 @@ var routes = [
     loginScreen: {
       component: LoginPage,
     },
+    beforeEnter: unauthenticated,
   },
 
   {
     path: '/stats/',
     component: StatsPage,
+    beforeEnter: authenticated,
   },
 
   {
     path: '/ideas/',
     component: IdeasPage,
+    beforeEnter: authenticated,
   },
 
   {
     path: '/services/',
     component: ServicesPage,
+    beforeEnter: authenticated,
   },
 
   {
     path: '/services/:id/',
     component: ServicePage,
+    beforeEnter: authenticated,
   },
 
   {
     path: '/coin/',
     component: CoinPage,
+    beforeEnter: authenticated,
   },
 
   {
     path: '/notifications/',
     component: NotificationsPage,
+    beforeEnter: authenticated,
   },
 
   {
     path: '/settings/',
     component: SettingsPage,
+    beforeEnter: authenticated,
   },
 
   {
     path: '/about/',
     component: AboutPage,
+    beforeEnter: authenticated,
   },
 
   {
