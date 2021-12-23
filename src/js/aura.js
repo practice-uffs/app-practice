99fe0f289fac9226326f7532242bf346e4859a3f
Stefani Meneghetti <meneghettistefani@gmail.com>
2021-12-23 09:34:19 -0300

    Envia a avaliação das respostas para a API

--- src/js/aura.js
+++ src/js/aura.js
@@ -16,6 +16,21 @@ export class Aura{
         }).catch(err => err);
     }
 
+    async rateAnswer(answer, value) {
+        var self = this;
+        var app = self.app;
+        const data = {
+            "app_id": 4,
+            "action": "aura_feedback",
+            "key": answer,
+            "value": value   
+        }
+        return await app.request.promise.postJSON(app.api.url + "analytics", data)
+        .then(async () => {
+            return true;
+        }).catch(err => false);
+    }
+
     setConsent(consent) {
         localStorage["auraConsent"] = JSON.stringify(consent);
     }
